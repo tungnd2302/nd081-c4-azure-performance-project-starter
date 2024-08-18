c39392d9620c4bef9dd17b7b453221ee32c1d5ea
Tung Nguyen <you@example.com>
2024-08-18 23:26:05 +0700

    update trace log

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -87,10 +87,10 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-        tracer.current_span.add_event("Cat votes reset", attributes={"vote_count": vote1})
+        tracer.span("Cat votes reset", attributes={"vote_count": vote1})
 
         vote2 = r.get(button2).decode('utf-8')
-        tracer.current_span.add_event("Dog votes reset", attributes={"vote_count": vote2})
+        tracer.span("Dog votes reset", attributes={"vote_count": vote2})
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
